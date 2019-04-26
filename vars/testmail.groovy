574bdd49ae0814fa9c8a88f873e653d6a7278e0f
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-26 03:16:42 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -2,5 +2,5 @@ def call(from , subject)
 	{
 	string Textpath = readFile 'C:\\Program Files (x86)\\Jenkins\\Text.txt'
 	//String[] lines = Textpath.text.split('\n')
-       // println "Total lines" + lines
+         println "Total lines" + Textpath
 	}
