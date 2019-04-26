95f3c0c354322efefbb8be0a6a124ea126cea978
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-25 23:52:59 -0700

    Create SentEmail.groovy

---
+++ src/Email/SentEmail.groovy
@@ -0,0 +1,16 @@
+package Email
+
+
+
+def Email(def SentEmail)
+
+{	
+
+}
+
+def SentEmail(from , to , subject , )
+
+	{
+	mail bcc: '', body: 'Testing', cc: '', from: 'deepak.kumar@ravsoftsolutions.com', replyTo: '', 
+  subject: 'Jenkins_Testing', to: 'sudhakar.anandan@ravsoftsolutions.com'
+	}
