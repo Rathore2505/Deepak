719917dcadb5af1df2fad821c05ef94cd52441e6
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-26 03:23:17 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -1,13 +1,12 @@
 def call(from , subject)
 	{
 	string Textpath = readFile 'C:\\Program Files (x86)\\Jenkins\\Text.txt'
-	 String[] lines = Textpath.split('\n')
+	String[] lines = Textpath.split('\n')
 	for(int i = 0; i < lines.size(); i++)
            {
-	     //mail bcc: '', body: 'Testing', cc: '', from: from , replyTo: '', 
-             //subject: subject, to: lines[i]
-		   //println "Mail ID : " + lines[i]
-		   println "text in line "+i.toString()+" : "+ lines[i]
+	     mail bcc: '', body: 'Testing', cc: '', from: from , replyTo: '', 
+             subject: subject, to: lines[i]
+	     println "text in line "+i.toString()+" : "+ lines[i]
 	   }	
          println "Total lines" + Textpath
 	}
