97bf7ea87dac8f57adb95ac21c160496e9ecdbf9
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-28 23:27:27 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -1,21 +1,49 @@
-import javax.mail.*
-import javax.mail.internet.*
-    
-def sendMail(host, sender, receivers, subject, text) 
+import hudson.model.*;
+import jenkins.model.*;
+import hudson.tools.*;
+import hudson.util.Secret;
+
+def sendMail(sender , receiver)
 {
-    Properties props = System.getProperties()
-    props.put("mail.smtp.host", host)
-    Session session = Session.getDefaultInstance(props, null)
+// Variables
+def SystemAdminMailAddress = env['deepak.kumar@ravsoftsolutions.com']
+def SMTPUser = env['deepak.kumar@ravsoftsolutions.com']
+def SMTPPassword = env['@deepak2505']
+def SMTPPort = env['465']
+def SMTPHost = env['secure200.inmotionhosting.com']
 
-    MimeMessage message = new MimeMessage(session)
-    message.setFrom(new InternetAddress(sender))
-    receivers.split(',').each {
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(it))
-    }
-    message.setSubject(subject)
-    message.setText(text)
+// Constants
+def instance = Jenkins.getInstance()
+def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
 
-    println 'Sending mail to ' + receivers + '.'
-    Transport.send(message)
-    println 'Mail sent.'
+Thread.start {
+    sleep 10000
+
+        //Jenkins Location
+        println "--> Configuring JenkinsLocation"
+        jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
+        jenkinsLocationConfiguration.save()
+
+        //E-mail Server
+        mailServer.setSmtpAuth(SMTPUser, SMTPPassword)
+        mailServer.setSmtpHost(SMTPHost)
+        mailServer.setSmtpPort(SMTPPort)
+        mailServer.setCharset("UTF-8")
+
+        //Extended-Email
+        extmailServer.smtpAuthUsername=SMTPUser
+        extmailServer.smtpAuthPassword=Secret.fromString(SMTPPassword)
+        extmailServer.smtpHost=SMTPHost
+        extmailServer.smtpPort=SMTPPort
+        extmailServer.charset="UTF-8"
+        extmailServer.defaultSubject="\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS!"
+        extmailServer.defaultBody="\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS:\n\nCheck console output at \$BUILD_URL to view the results."
+
+    // Save the state
+    instance.save()
+} 
+    mail bcc: '', body: 'Testing', cc: '', from: sender, replyTo: '', 
+ subject: 'Jenkins_Testing', to: receiver
 }
