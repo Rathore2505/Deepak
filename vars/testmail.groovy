62e1736f9b16139a9fbbce45e244a0029ed61fda
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-29 02:04:05 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -1,40 +1,22 @@
-import hudson.model.*;
-import jenkins.model.*;
-import hudson.tools.*;
-import hudson.util.Secret;
-
-def call(receiver)
+import javax.mail.*
+import javax.mail.internet.*
+    
+def call(host, sender, receivers, subject, text) 
 {
-// Variables
-def SystemAdminMailAddress = env['deepak.kumar@ravsoftsolutions.com']
-def SMTPUser = env['deepak.kumar@ravsoftsolutions.com']
-def SMTPPassword = env['@deepak2505']
-def SMTPPort = env['465']
-def SMTPHost = env['secure200.inmotionhosting.com']
+    Properties props = System.getProperties()
+    props.put("mail.smtp.host", host)
+    Session session = Session.getDefaultInstance(props, null)
 
-// Constants
-def instance = Jenkins.getInstance()
-def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
-def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
-Thread.start {
-    sleep 10000
-        //Jenkins Location
-        println "--> Configuring JenkinsLocation"
-        jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
-        jenkinsLocationConfiguration.save()
+    MimeMessage message = new MimeMessage(session)
+    message.setFrom(new InternetAddress(sender))
+    receivers.split(',').each {
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(it))
+ }
+    message.setSubject(subject)
+    message.setText(text)
 
-        //E-mail Server
-        mailServer.setSmtpAuth(SMTPUser, SMTPPassword)
-        mailServer.setSmtpHost(SMTPHost)
-        mailServer.setSmtpPort(SMTPPort)
-        mailServer.setCharset("UTF-8")
-    // Save the state
-        instance.save()
-} 
-    // Send mail
-        mail bcc: '', body: 'Testing', cc: '', from: SystemAdminMailAddress, replyTo: '', 
-            subject: 'Jenkins', to: receiver
-       println 'Mail sent.'
-   
+    println 'Sending mail to ' + receivers + '.'
+    Transport.send(message)
+    println 'Mail sent.'
+    
 }
