a54ef6f8c654fbab6af65224a16249128803e78a
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-29 01:50:54 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -17,8 +17,7 @@ def instance = Jenkins.getInstance()
 def mailServer = instance.getDescriptor("hudson.tasks.Mailer")
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
 def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmailPublisher")
-Thread.start {
-    sleep 10000
+
         //Jenkins Location
         println "--> Configuring JenkinsLocation"
         jenkinsLocationConfiguration.setAdminAddress(SystemAdminMailAddress)
@@ -29,21 +28,10 @@ Thread.start {
         mailServer.setSmtpHost(SMTPHost)
         mailServer.setSmtpPort(SMTPPort)
         mailServer.setCharset("UTF-8")
-
-        //Extended-Email
-        //extmailServer.smtpAuthUsername=SMTPUser
-        //extmailServer.smtpAuthPassword=Secret.fromString(SMTPPassword)
-        //extmailServer.smtpHost=SMTPHost
-        //extmailServer.smtpPort=SMTPPort
-        //extmailServer.charset="UTF-8"
-        //extmailServer.defaultSubject="\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS!"
-        //extmailServer.defaultBody="\$PROJECT_NAME - Build # \$BUILD_NUMBER - \$BUILD_STATUS:\n\nCheck console output at \$BUILD_URL to view the results."
-
     // Save the state
         instance.save()
-}
        
-        // Send mail
+    // Send mail
         mail bcc: '', body: 'Testing', cc: '', from: from, replyTo: '', 
             subject: 'Jenkins', to: receiver
        println 'Mail sent.'
