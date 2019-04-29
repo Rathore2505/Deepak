1b1fbcbdc6690e3a587cce2e2345617cf019b52f
Rathore2505 <49403036+Rathore2505@users.noreply.github.com>
2019-04-29 03:51:20 -0700

    Update testmail.groovy

--- vars/testmail.groovy
+++ vars/testmail.groovy
@@ -11,8 +11,8 @@ def call(sender, receivers, subject, text)
 def SystemAdminMailAddress = env['deepak.kumar@ravsoftsolutions.com']
 def SMTPUser = env['deepak.kumar@ravsoftsolutions.com']
 def SMTPPassword = env['@deepak2505']
-def SMTPPort = env['995']
-def SMTPHost = env['198.46.81.32']
+def SMTPPort = env['465']
+def SMTPHost = env['mail.ravsoftsolutions.com']
 
 // Constants
 def instance = Jenkins.getInstance()
@@ -34,8 +34,8 @@ def extmailServer = instance.getDescriptor("hudson.plugins.emailext.ExtendedEmai
         instance.save() 
  
     Properties props = System.getProperties()
-    props.put("mail.smtp.host", '198.46.81.32')
-    props.put("mail.smtp.port", '995');
+    props.put("mail.smtp.host", 'mail.ravsoftsolutions.com')
+    props.put("mail.smtp.port", '465');
      Session session = Session.getDefaultInstance(props, null)
     MimeMessage message = new MimeMessage(session)
     message.setFrom(new InternetAddress(sender))
